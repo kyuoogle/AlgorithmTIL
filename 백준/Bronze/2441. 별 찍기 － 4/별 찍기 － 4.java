b55631aecf6246199d9473e0a0d5ebeab144b24d
Youngkyu Kim <goodboy19990@naver.com>
2025-08-19 17:39:10 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 208 ms, Memory: 15780 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,20 @@
+import java.io.*;
+//import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 0; k < N - i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
