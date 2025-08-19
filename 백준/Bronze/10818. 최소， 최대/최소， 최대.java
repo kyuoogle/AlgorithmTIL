f43bc3ab02bda66e3ccb5cda18fbfe9ceefcd6a4
Youngkyu Kim <goodboy19990@naver.com>
2025-08-19 16:52:02 +0900

    [Bronze III] Title: 최소, 최대, Time: 1220 ms, Memory: 109920 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] nums = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		
+		sb.append(nums[0]).append(" ").append(nums[N - 1]);
+		
+		System.out.println(sb.toString());
+		
+	}
+}
