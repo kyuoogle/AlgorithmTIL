832e55f5847121a30314c5f3857bb7f734812218
Youngkyu Kim <goodboy19990@naver.com>
2025-08-20 12:59:20 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 28964 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		
+		for(int i = 1; i <= N; i++) {
+			q.add(i);
+		}
+		
+		while(q.size() > 1) {
+			q.pollFirst();
+			q.offerLast(q.pollFirst());
+		}
+		
+		System.out.println(q.peekFirst());
+		
+	}
+}
