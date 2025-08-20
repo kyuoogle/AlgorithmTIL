629e84fe079d5058973f9be0c8be75852f6e8894
Youngkyu Kim <goodboy19990@naver.com>
2025-08-20 15:11:02 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4804 ms, Memory: 184228 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] nums = new int [N];
+        
+        for(int i = 0; i < N; i++) {
+        	nums[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < N; i++) {
+        	System.out.println(nums[i]);
+        }
+        
+    }
+}
