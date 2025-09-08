68f98755a9f2e03cd537757fc744f0c629c52371
Youngkyu Kim <goodboy19990@naver.com>
2025-09-08 13:01:08 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 23488 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static int[] arr;
+	public static boolean[] visited;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+				
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		visited = new boolean[N];
+		dfs(N, M, 0);
+		System.out.println(sb);
+	}
+
+	private static void dfs(int N, int M, int depth) {
+		// 기저 조건
+		if(depth == M) {
+			for(int val: arr) {
+				sb.append(val).append(" ");
+			}
+			sb.append("\n"); 
+			return;
+		}
+		for(int i = 0; i < N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i + 1;
+				dfs(N, M, depth + 1);
+				visited[i] = false;
+			}
+		}
+	}
+}
