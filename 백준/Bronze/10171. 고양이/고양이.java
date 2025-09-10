28c9692e1db099b20e8f25ebab8c45e9835ea64c
Youngkyu Kim <goodboy19990@naver.com>
2025-09-10 14:50:30 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		String input = "\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|";
+		
+		System.out.print(input);
+	}
+}
