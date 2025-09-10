9663efde87d6a3fd5dfe9650933bc70c3363eeab
Youngkyu Kim <goodboy19990@naver.com>
2025-09-10 15:06:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		String line = br.readLine();
+		
+		char ch = line.charAt(0);
+		
+		System.out.println((int)ch);
+		
+		
+	}
+}
