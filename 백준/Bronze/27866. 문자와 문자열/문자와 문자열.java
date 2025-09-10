0397b132c588d3ddc63652ca295aad50530648f2
Youngkyu Kim <goodboy19990@naver.com>
2025-09-10 15:10:05 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		String line = br.readLine();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		char ch = line.charAt(N - 1);
+		
+		System.out.println(ch);
+		
+	}
+}
