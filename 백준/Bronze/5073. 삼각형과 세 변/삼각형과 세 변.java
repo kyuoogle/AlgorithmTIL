05cf748758063c0128b8731868e9b7c20d1b049d
Youngkyu Kim <goodboy19990@naver.com>
2025-09-12 15:18:34 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//무한루프
+		while(true) {
+			int[] arr = new int[3];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[0] = Integer.parseInt(st.nextToken());
+			arr[1] = Integer.parseInt(st.nextToken());
+			arr[2] = Integer.parseInt(st.nextToken());
+			
+			//0 0 0 입력시 무한루프 종료
+			if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+			
+			//정렬
+			Arrays.sort(arr);
+			
+			if(arr[2] >= arr[0] + arr[1]) {
+				System.out.println("Invalid");
+			}else if(arr[0] == arr[1] && arr[1] == arr[2]) {
+				System.out.println("Equilateral");
+			}else if(arr[0] == arr[1] || arr[1] == arr[2] || arr[0] == arr[2]) {
+				System.out.println("Isosceles");
+			}else {
+				System.out.println("Scalene");
+			}
+		}
+	}
+}
