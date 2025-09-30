9d740e6e1da840692c0db91b0c0ff35c61f6fdec
Youngkyu Kim <goodboy19990@naver.com>
2025-09-30 15:33:31 +0900

    [Silver V] Title: 숫자 카드, Time: 872 ms, Memory: 135192 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            sb.append(set.contains(x) ? 1 : 0).append(' ');
+        }
+        System.out.println(sb.toString());
+    }
+}
