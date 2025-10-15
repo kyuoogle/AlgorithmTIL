f1a35160f9e1cec7c03b5a5be63dce776368ed55
Youngkyu Kim <goodboy19990@naver.com>
2025-10-15 13:19:56 +0900

    [Bronze IV] Title: 세수정렬, Time: 116 ms, Memory: 15880 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] nums = new int[3];
+        
+        for(int i = 0; i < 3; i++) {
+        	nums[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < 3; i++) {
+        	System.out.print(nums[i] + " ");
+        }
+    }
+}
