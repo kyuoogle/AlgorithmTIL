8cd17a399fa75ce51b652a07305177cf87067b7e
Youngkyu Kim <goodboy19990@naver.com>
2025-10-18 23:41:14 +0900

    [Bronze II] Title: 농구 경기, Time: 96 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] count = new int[26]; // 알파벳 등장 횟수
+
+        for (int i = 0; i < N; i++) {
+            String name = br.readLine();
+            char first = name.charAt(0);
+            count[first - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (count[i] >= 5) {
+                sb.append((char)(i + 'a'));
+            }
+        }
+
+        if (sb.length() == 0) {
+            System.out.println("PREDAJA");
+        } else {
+            System.out.println(sb.toString());
+        }
+    }
+}
