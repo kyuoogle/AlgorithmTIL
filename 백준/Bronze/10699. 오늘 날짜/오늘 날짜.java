09efa3f02d35e709378d9894dbdc2fc6d25973c5
YoungKyu Kim <goodboy19990@naver.com>
2025-11-20 17:35:47 +0900

    [Bronze V] Title: 오늘 날짜, Time: 164 ms, Memory: 17100 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,13 @@
+import java.time.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		LocalDate now = LocalDate.now();
+		System.out.println(now);
+		
+		
+	}// main()
+	
+}// class Main
