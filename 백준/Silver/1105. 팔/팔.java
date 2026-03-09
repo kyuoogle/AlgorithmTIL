5caf8f8e0949ab8273e22112dd13672c5a683179
YoungKyu Kim <goodboy19990@naver.com>
2026-03-09 18:59:57 +0900

    [Silver I] Title: 팔, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String L = st.nextToken();
+        String R = st.nextToken();
+
+        if (L.length() != R.length()) {
+            System.out.println(0);
+            return;
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < L.length(); i++) {
+            if (L.charAt(i) != R.charAt(i)) break;
+            if (L.charAt(i) == '8') cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
