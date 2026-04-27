48ba77d64887ba3d7562d8b5a341376b47090217
YoungKyu Kim <goodboy19990@naver.com>
2026-04-27 15:27:30 +0900

    [level 2] Title: 올바른 괄호, Time: 5.68 ms, Memory: 55 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    boolean solution(String s) {
+        int cnt = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if(c == '(') {
+                cnt = cnt + 1;
+            } else {
+                cnt = cnt - 1;
+            }
+            if(cnt < 0) return false;
+        }
+        
+        if(cnt == 0) return true;
+        else return false;
+    }
+}
