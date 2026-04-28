ea877cc662f7a8d79e64857b4f418180e002d8a5
YoungKyu Kim <goodboy19990@naver.com>
2026-04-28 18:10:58 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int total = brown + yellow;
+        
+        for(int h = 3; h <= total; h++) {
+            if(total % h != 0) continue;
+            
+            int w = total / h;
+            
+            if((w-2) * (h-2) == yellow) return new int[] {w, h};
+        }
+        return new int[]{0, 0};
+    }
+}
