6d6c0a1b0c3a1df337a44f94e741f97bbe249192
YoungKyu Kim <goodboy19990@naver.com>
2026-04-28 20:08:57 +0900

    [level 2] Title: 큰 수 만들기, Time: 34.12 ms, Memory: 98.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < number.length(); i++) {
+            char current = number.charAt(i);
+
+            while (k > 0 && sb.length() > 0 && sb.charAt(sb.length() - 1) < current) {
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
+            }
+
+            sb.append(current);
+        }
+
+        if (k > 0) {
+            sb.delete(sb.length() - k, sb.length());
+        }
+
+        return sb.toString();
+    }
+}
