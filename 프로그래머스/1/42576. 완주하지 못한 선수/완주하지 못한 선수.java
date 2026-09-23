61f3dfdd1b1b2f3a8d808201db7353c2137742a1
YoungKyu Kim <goodboy19990@naver.com>
2026-09-23 17:34:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 207.92 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+
+    public String solution(String[] p, String[] c) {
+        Arrays.sort(p);
+        Arrays.sort(c);
+        
+        for(int i = 0; i < c.length; i++) {
+            if(!p[i].equals(c[i])) return p[i];
+        }
+        
+        return p[p.length - 1];
+    }
+}
